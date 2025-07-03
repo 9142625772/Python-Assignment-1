40e72998e0480edd9e3c77a46556aa7fe6b8c5da
9142625772 <Anshkr91426@gmail.com>
2025-07-03 22:37:55 +0530

    Add files via upload

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,46 @@
+#Module 2 : Basic python concept
+#Task-1
+#Problem Statement: Write a Python program that does the following:
+
+#1. Takes two numbers as input from the user.
+a=int(input("enter the first value:"))
+b=int(input("enter tge second value:"))
+
+#2.Performs the basic mathematical operations on these tow numbers:
+    
+a=20
+b=10
+
+#o Addition
+
+c=(a+b)
+
+#o Subtraction
+
+d=(a-b)
+
+#o Multiplication
+
+e=(a*b)
+
+#o Division
+f=(a/b)
+
+#3.Displays the results of each operation on the screen.
+print(c)
+print(d)
+print(e)
+print(f)
+
+
+#Task 2.
+#Problem Statement: Write a Python program that:
+
+#1. Takes a user's first name and last name as input.
+first_name=input("Enter your first name:")
+second_name=input("Enter your second name:")
+#2. Concatenates the first name and last name into a full name.
+print(first_name+" "+second_name)
+
+#3. Prints a personalized greeting message using the full name.
+print("Hello "+first_name,second_name+'!'+" welcome to python program.")
